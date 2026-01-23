493d99b6ca3304832851a8c2610b03b07ec84251
CPuddingOwO <150633243+CPuddingOwO@users.noreply.github.com>
2026-01-24 07:09:13 +0800

    Modify: 分离Extensions中的扩展到各个文件

---
+++ Helpers/AnyArrayExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Cornifer.Helpers;
+
+public static class AnyArrayExtensions {
+    public static bool TryGet<T>(this T[] array, int index, out T value) {
+        if (index < array.Length) {
+            value = array[index];
+            return true;
+        }
+
+        value = default!;
+        return false;
+    }
+}
---
+++ Helpers/ColorExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Cornifer.Helpers;
+
+public static class ColorExtensions {
+    public static void SetAlpha(ref this Color color, float alpha) {
+        color.A = (byte)(Math.Clamp(alpha, 0, 1) * 255);
+    }
+}
