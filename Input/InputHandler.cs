67f629945e4a1462a58f78a9aa3ea90923101a46
CPuddingOwO <150633243+CPuddingOwO@users.noreply.github.com>
2026-01-24 07:03:13 +0800

    Append: Keybind 和 InputHandler

--- Input/InputHandler.cs
+++ Input/InputHandler.cs
@@ -1,25 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace Cornifer.Input;
 
 public static class InputHandler {
-    
     public static KeyboardState KeyboardState, PrevKeyboardState;
     public static MouseState MouseState, PrevMouseState;
-    
-    public static Keys[] AllKeys = Enum.GetValues<Keys>();
-    public static MouseKeys[] AllMouseKeys = Enum.GetValues<MouseKeys>();
-    
-    
-    
-    public static void Initialize() { }
+
+    public static bool Disable => false;
+    public static Dictionary<string, Keybind> Keybinds = new();
+
+    // 快捷获取坐标属性
+    public static Point MousePoint => MouseState.Position;
+    public static Vector2 MousePosition => new Vector2(MouseState.X, MouseState.Y);
+    public static Vector2 MouseDelta => (MouseState.Position - PrevMouseState.Position).ToVector2();
+
+    // Keybinds
+    // public static Keybind Undo = new("Undo", ModifierKeys.Control, Keys.Z);
+    // public static Keybind Redo = new("Redo", ModifierKeys.Control, Keys.Y);
+
+    public static readonly Keybind Select = new("Select", MouseKeys.LeftButton);
+    public static readonly Keybind Move = new("Move", MouseKeys.RightButton);
+
+
+    public static void Initialize() {
+        // 自动注册静态字段中的 Keybind
+        var fields = typeof(InputHandler).GetFields(BindingFlags.Public | BindingFlags.Static);
+        foreach (var f in fields.Where(f => f.FieldType == typeof(Keybind))) {
+            Keybinds[f.Name] = (Keybind)f.GetValue(null)!;
+        }
+        // LoadKeybinds();
+    }
 
     public static void Update() {
         PrevMouseState = MouseState;
         MouseState = Mouse.GetState();
 
         PrevKeyboardState = KeyboardState;
-        KeyboardState = Keyboard.GetState();   
+        KeyboardState = Keyboard.GetState();
+    }
+
+    public static void RefreshEncapsulatedBinds() {
+        var allCombos = Keybinds.Values.SelectMany(k => k.Inputs).ToList();
+        foreach (var combo in allCombos) {
+            combo.EncapsulatingCombos.Clear();
+            // 如果 comboB 包含 comboA 的所有键且比 comboA 更长，则 comboB 封装 comboA
+            var superiors = allCombos.Where(other => other.ComboEncapsulates(combo));
+            foreach (var s in superiors)
+                if (!combo.EncapsulatingCombos.Any(existing => existing.InputEquality(s)))
+                    combo.EncapsulatingCombos.Add(s);
+        }
     }
 }
---
+++ Input/InputSources.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace Cornifer.Input {
+    // 键盘输入
+    public class KeyboardInput(Keys key) : KeybindInput {
+        public Keys Key { get; set; } = key;
+        public override bool CurrentState => !InputHandler.Disable && InputHandler.KeyboardState.IsKeyDown(Key);
+        public override bool PrevState => !InputHandler.Disable && InputHandler.PrevKeyboardState.IsKeyDown(Key);
+        public override string KeyName => Key.ToString();
+    }
+
+    // 修饰键输入 (Shift/Ctrl/Alt)
+    public class ModifierInput(ModifierKeys key) : KeybindInput {
+        public ModifierKeys Key { get; set; } = key;
+        public override bool CurrentState => !InputHandler.Disable && GetState(InputHandler.KeyboardState);
+        public override bool PrevState => !InputHandler.Disable && GetState(InputHandler.PrevKeyboardState);
+        public override string KeyName => Key.ToString();
+
+        private bool GetState(KeyboardState state) => Key switch {
+            ModifierKeys.Shift => state.IsKeyDown(Keys.LeftShift) || state.IsKeyDown(Keys.RightShift),
+            ModifierKeys.Control => state.IsKeyDown(Keys.LeftControl) || state.IsKeyDown(Keys.RightControl),
+            ModifierKeys.Alt => state.IsKeyDown(Keys.LeftAlt) || state.IsKeyDown(Keys.RightAlt),
+            ModifierKeys.Windows => state.IsKeyDown(Keys.LeftWindows) || state.IsKeyDown(Keys.RightWindows),
+            _ => false
+        };
+    }
+
+    // 鼠标输入
+    public class MouseInput(MouseKeys key) : KeybindInput {
+        public MouseKeys Key { get; set; } = key;
+        public override bool CurrentState => !InputHandler.Disable && GetState(InputHandler.MouseState);
+        public override bool PrevState => !InputHandler.Disable && GetState(InputHandler.PrevMouseState);
+        public override string KeyName => Key.ToString();
+
+        private bool GetState(MouseState state) => Key switch {
+            MouseKeys.LeftButton => state.LeftButton == ButtonState.Pressed,
+            MouseKeys.RightButton => state.RightButton == ButtonState.Pressed,
+            MouseKeys.MiddleButton => state.MiddleButton == ButtonState.Pressed,
+            _ => false
+        };
+    }
+}
