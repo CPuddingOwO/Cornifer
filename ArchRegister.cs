a2cc4b93f8aa7f8799532bb7f820951a33a168ca
CPuddingOwO <150633243+CPuddingOwO@users.noreply.github.com>
2026-01-24 07:29:11 +0800

    Append: Arch System

--- ArchRegister.cs
+++ ArchRegister.cs
@@ -1,13 +1,18 @@
-﻿using Cornifer.Systems;
+﻿using Cornifer.Renderers;
+using Cornifer.Systems;
+using Microsoft.Xna.Framework;
 
 namespace Cornifer;
 
 public static class ArchRegister {
     public static void Update() {
-        HierarchySystem.Update(Map.World);   
+        HierarchySystem.Update(Map.World);
+        // 重建四叉树 (假设地图边界为 0,0,5000,5000)
+        SpatialSystem.RebuildIndex(Map.World, new Rectangle(0, 0, 5000, 5000));
     }
     
-    public static void Draw() {
-        RenderSystem.Draw(Map.World);
+    public static void Draw(Renderer renderer) {
+        VisualSystem.Draw(Map.World, renderer);
+        GizmoSystem.Draw(renderer, Map.SelectedEntity);
     }
 }
